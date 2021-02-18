hanzoai/metrics

commit 0fdafddd103115a80a83c0e14d3ae8a4fb09d519

adds pushback for fastqueue, (#1075)
during shutdown currently sending block was lost,
now its pushed back to fast queue and will be flushed on disk,
it may lead to data duplication.
https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1065

app/vmagent/remotewrite/remotewrite.go
@@ -227,10 +227,10 @@ func (rwctx *remoteWriteCtx) MustStop() {
 	}
 	rwctx.idx = 0
 	rwctx.pss = nil
-	rwctx.fq.MustClose()
-	rwctx.fq = nil
 	rwctx.c.MustStop()
 	rwctx.c = nil
+	rwctx.fq.MustClose()
+	rwctx.fq = nil
 
 	rwctx.relabelMetricsDropped = nil
 }

app/vmagent/remotewrite/client.go
@@ -178,7 +178,9 @@ func (c *client) runWorker() {
 			return
 		}
 		go func() {
-			c.sendBlock(block)
+			if delivered := c.sendBlockOk(block); !delivered {
+				return
+			}
 			ch <- struct{}{}
 		}()
 		select {
@@ -190,17 +192,17 @@ func (c *client) runWorker() {
 			graceDuration := 5 * time.Second
 			select {
 			case <-ch:
+				logger.Infof("stop ok")
 				// The block has been sent successfully.
 			case <-time.After(graceDuration):
-				logger.Errorf("couldn't sent block with size %d bytes to %q in %.3f seconds during shutdown; dropping it",
-					len(block), c.sanitizedURL, graceDuration.Seconds())
+				c.fq.MustWriteBlock(block)
 			}
 			return
 		}
 	}
 }
 
-func (c *client) sendBlock(block []byte) {
+func (c *client) sendBlockOk(block []byte) bool {
 	c.rl.register(len(block), c.stopCh)
 	retryDuration := time.Second
 	retriesCount := 0
@@ -236,7 +238,7 @@ again:
 		select {
 		case <-c.stopCh:
 			timerpool.Put(t)
-			return
+			return false
 		case <-t.C:
 			timerpool.Put(t)
 		}
@@ -247,7 +249,7 @@ again:
 	if statusCode/100 == 2 {
 		_ = resp.Body.Close()
 		c.requestsOKCount.Inc()
-		return
+		return true
 	}
 	metrics.GetOrCreateCounter(fmt.Sprintf(`vmagent_remotewrite_requests_total{url=%q, status_code="%d"}`, c.sanitizedURL, statusCode)).Inc()
 	if statusCode == 409 {
@@ -258,7 +260,7 @@ again:
 		logger.Errorf("unexpected status code received when sending a block with size %d bytes to %q: #%d; dropping the block like Prometheus does; "+
 			"response body=%q", len(block), c.sanitizedURL, statusCode, body)
 		c.packetsDropped.Inc()
-		return
+		return true
 	}
 
 	// Unexpected status code returned
@@ -279,7 +281,7 @@ again:
 	select {
 	case <-c.stopCh:
 		timerpool.Put(t)
-		return
+		return false
 	case <-t.C:
 		timerpool.Put(t)
 	}

commit 1198cd105e8b5c9d61ff642e382beb5ab496287e

app/vmagent: properly perform graceful shutdown, which was broken in the commit 0fdafddd103115a80a83c0e14d3ae8a4fb09d519
Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1065

app/vmagent/remotewrite/remotewrite.go
@@ -227,6 +227,7 @@ func (rwctx *remoteWriteCtx) MustStop() {
 	}
 	rwctx.idx = 0
 	rwctx.pss = nil
+	rwctx.fq.UnblockAllReaders()
 	rwctx.c.MustStop()
 	rwctx.c = nil
 	rwctx.fq.MustClose()

lib/persistentqueue/fastqueue.go
@@ -52,16 +52,24 @@ func MustOpenFastQueue(path, name string, maxInmemoryBlocks, maxPendingBytes int
 	return fq
 }
 
-// MustClose unblocks all the readers.
-//
-// It is expected no new writers during and after the call.
-func (fq *FastQueue) MustClose() {
+// UnblockAllReaders unblocks all the readers.
+func (fq *FastQueue) UnblockAllReaders() {
 	fq.mu.Lock()
 	defer fq.mu.Unlock()
 
 	// Unblock blocked readers
 	fq.mustStop = true
 	fq.cond.Broadcast()
+}
+
+// MustClose unblocks all the readers.
+//
+// It is expected no new writers during and after the call.
+func (fq *FastQueue) MustClose() {
+	fq.UnblockAllReaders()
+
+	fq.mu.Lock()
+	defer fq.mu.Unlock()
 
 	// flush blocks from fq.ch to fq.pq, so they can be persisted
 	fq.flushInmemoryBlocksToFileLocked()