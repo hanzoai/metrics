hanzoai/metrics

commit da65ed4d640d3beaf0a9c01cfecdb3f41e33fd02

lib/promscrape/discovery/kubernetes: refresh `role: endpoints` targets on service object removal as Prometheus does
This is a follow-up for fe7f31d059d9b10fac1755a913a4e885b85f5916

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240

lib/promscrape/discovery/kubernetes/api_watcher.go
@@ -301,6 +301,13 @@ func (gw *groupWatcher) getCachedObjectByRole(role, namespace, name string) obje
 	return nil
 }
 
+func (gw *groupWatcher) refreshEndpointsLabels(namespace, key string) {
+	// Refresh endpoints and endpointslices labels for the corresponding service as Prometheus does.
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240
+	gw.refreshObjectLabels("endpoints", namespace, key)
+	gw.refreshObjectLabels("endpointslices", namespace, key)
+}
+
 func (gw *groupWatcher) refreshObjectLabels(role, namespace, key string) {
 	// There is no need in starting url watcher for the given role,
 	// since there is no (namespace, key) object yet for this role.
@@ -550,13 +557,10 @@ func (uw *urlWatcher) reloadObjects() string {
 
 	uw.reloadScrapeWorksForAPIWatchers(aws, objectsByKey)
 	if uw.role == "service" {
-		// Update endpoints and endpointslices for the corresponding service as Prometheus does.
+		// Refresh endpoints labels for the corresponding services as Prometheus does.
 		// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240
-		gw := uw.gw
-		namespace := uw.namespace
 		for key := range objectsByKey {
-			gw.refreshObjectLabels("endpoints", namespace, key)
-			gw.refreshObjectLabels("endpointslices", namespace, key)
+			uw.gw.refreshEndpointsLabels(uw.namespace, key)
 		}
 	}
 	logger.Infof("reloaded %d objects from %q", len(objectsByKey), requestURL)
@@ -686,10 +690,9 @@ func (uw *urlWatcher) readObjectUpdateStream(r io.Reader) error {
 				}
 			}
 			if uw.role == "service" {
-				// Update endpoints and endpointslices for the corresponding service as Prometheus does.
+				// Refresh endpoints labels for the corresponding service as Prometheus does.
 				// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240
-				uw.gw.refreshObjectLabels("endpoints", uw.namespace, key)
-				uw.gw.refreshObjectLabels("endpointslices", uw.namespace, key)
+				uw.gw.refreshEndpointsLabels(uw.namespace, key)
 			}
 		case "DELETED":
 			o, err := uw.parseObject(we.Object)
@@ -707,6 +710,11 @@ func (uw *urlWatcher) readObjectUpdateStream(r io.Reader) error {
 			uw.mu.Unlock()
 			for _, aw := range aws {
 				aw.removeScrapeWorks(uw.namespace, key)
+			}
+			if uw.role == "service" {
+				// Refresh endpoints labels for the corresponding service as Prometheus does.
+				// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240
+				uw.gw.refreshEndpointsLabels(uw.namespace, key)
 			}
 		case "BOOKMARK":
 			// See https://kubernetes.io/docs/reference/using-api/api-concepts/#watch-bookmarks

commit 3413cfa00c9fb7ce37f76dc98fe352b6c8fb6371

lib/promscrape/discovery/kubernetes: fix a deadlock introduced in da65ed4d640d3beaf0a9c01cfecdb3f41e33fd02
Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/1240

Thanks to @f41gh7 for providing the initial idea for deadlock fix at https://github.com/VictoriaMetrics/VictoriaMetrics/pull/1248

lib/promscrape/discovery/kubernetes/api_watcher.go
@@ -74,6 +74,11 @@ func newAPIWatcher(apiServer string, ac *promauth.Config, sdc *SDConfig, swcFunc
 
 func (aw *apiWatcher) mustStart() {
 	aw.gw.startWatchersForRole(aw.role, aw)
+	if aw.role == "endpoints" || aw.role == "endpointslices" {
+		// endpoints and endpointslices watchers query pod and service objects. So start watchers for these roles as well.
+		aw.gw.startWatchersForRole("pod", nil)
+		aw.gw.startWatchersForRole("service", nil)
+	}
 }
 
 func (aw *apiWatcher) mustStop() {
@@ -280,7 +285,6 @@ func (gw *groupWatcher) getObjectByRole(role, namespace, name string) object {
 
 func (gw *groupWatcher) getCachedObjectByRole(role, namespace, name string) object {
 	key := namespace + "/" + name
-	gw.startWatchersForRole(role, nil)
 	uws := gw.getURLWatchers()
 	for _, uw := range uws {
 		if uw.role != role {
@@ -309,9 +313,6 @@ func (gw *groupWatcher) refreshEndpointsLabels(namespace, key string) {
 }
 
 func (gw *groupWatcher) refreshObjectLabels(role, namespace, key string) {
-	// There is no need in starting url watcher for the given role,
-	// since there is no (namespace, key) object yet for this role.
-	// gw.startWatchersForRole(role, nil)
 	uws := gw.getURLWatchers()
 	for _, uw := range uws {
 		if uw.role != role {
@@ -470,6 +471,10 @@ func (uw *urlWatcher) subscribeAPIWatcher(aw *apiWatcher) {
 
 func (uw *urlWatcher) registerPendingAPIWatchers() {
 	uw.mu.Lock()
+	if len(uw.awsPending) == 0 {
+		uw.mu.Unlock()
+		return
+	}
 	awsPending := make([]*apiWatcher, 0, len(uw.awsPending))
 	for aw := range uw.awsPending {
 		awsPending = append(awsPending, aw)