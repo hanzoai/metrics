hanzoai/metrics

commit dfc26f3ea3c3862a626d703bc9829c0c6e857455

lib/{storage,mergeset}: convert InitFromFilePart to MustInitFromFilePart
Callers of InitFromFilePart log the error and exit.
It is better to log the error with the path to the part and the call stack
directly inside the MustInitFromFilePart() function.
This simplifies the code at callers' side while leaving the same level of debuggability.

lib/storage/part_header.go
@@ -131,7 +131,7 @@ func (ph *partHeader) ParseFromPath(path string) error {
 	return nil
 }
 
-func (ph *partHeader) ReadMetadata(partPath string) error {
+func (ph *partHeader) MustReadMetadata(partPath string) {
 	ph.Reset()
 
 	metadataPath := filepath.Join(partPath, metadataFilename)
@@ -140,29 +140,29 @@ func (ph *partHeader) ReadMetadata(partPath string) error {
 		if os.IsNotExist(err) {
 			// This is a part created before v1.90.0.
 			// Fall back to reading the metadata from the partPath itsel
-			return ph.ParseFromPath(partPath)
+			if err := ph.ParseFromPath(partPath); err != nil {
+				logger.Panicf("FATAL: cannot parse metadata from %q: %s", partPath, err)
+			}
 		}
-		return fmt.Errorf("cannot read %q: %w", metadataPath, err)
+		logger.Panicf("FATAL: cannot read %q: %s", metadataPath, err)
 	}
 	if err := json.Unmarshal(metadata, ph); err != nil {
-		return fmt.Errorf("cannot parse %q: %w", metadataPath, err)
+		logger.Panicf("FATAL: cannot parse %q: %s", metadataPath, err)
 	}
 
 	// Perform various checks
 	if ph.MinTimestamp > ph.MaxTimestamp {
-		return fmt.Errorf("minTimestamp cannot exceed maxTimestamp; got %d vs %d", ph.MinTimestamp, ph.MaxTimestamp)
+		logger.Panicf("FATAL: minTimestamp cannot exceed maxTimestamp at %q; got %d vs %d", metadataPath, ph.MinTimestamp, ph.MaxTimestamp)
 	}
 	if ph.RowsCount <= 0 {
-		return fmt.Errorf("rowsCount must be greater than 0; got %d", ph.RowsCount)
+		logger.Panicf("FATAL: rowsCount must be greater than 0 at %q; got %d", metadataPath, ph.RowsCount)
 	}
 	if ph.BlocksCount <= 0 {
-		return fmt.Errorf("blocksCount must be greater than 0; got %d", ph.BlocksCount)
+		logger.Panicf("FATAL: blocksCount must be greater than 0 at %q; got %d", metadataPath, ph.BlocksCount)
 	}
 	if ph.BlocksCount > ph.RowsCount {
-		return fmt.Errorf("blocksCount cannot be bigger than rowsCount; got blocksCount=%d, rowsCount=%d", ph.BlocksCount, ph.RowsCount)
+		logger.Panicf("FATAL: blocksCount cannot be bigger than rowsCount at %q; got blocksCount=%d, rowsCount=%d", metadataPath, ph.BlocksCount, ph.RowsCount)
 	}
-
-	return nil
 }
 
 func (ph *partHeader) MustWriteMetadata(partPath string) {

lib/mergeset/part_header.go
@@ -78,39 +78,37 @@ func (ph *partHeader) CopyFrom(src *partHeader) {
 	ph.lastItem = append(ph.lastItem[:0], src.lastItem...)
 }
 
-func (ph *partHeader) ReadMetadata(partPath string) error {
+func (ph *partHeader) MustReadMetadata(partPath string) {
 	ph.Reset()
 
 	// Read ph fields from metadata.
 	metadataPath := filepath.Join(partPath, metadataFilename)
 	metadata, err := os.ReadFile(metadataPath)
 	if err != nil {
-		return fmt.Errorf("cannot read %q: %w", metadataPath, err)
+		logger.Panicf("FATAL: cannot read %q: %s", metadataPath, err)
 	}
 
 	var phj partHeaderJSON
 	if err := json.Unmarshal(metadata, &phj); err != nil {
-		return fmt.Errorf("cannot parse %q: %w", metadataPath, err)
+		logger.Panicf("FATAL: cannot parse %q: %s", metadataPath, err)
 	}
 
 	if phj.ItemsCount <= 0 {
-		return fmt.Errorf("part %q cannot contain zero items", partPath)
+		logger.Panicf("FATAL: part %q cannot contain zero items", partPath)
 	}
 	ph.itemsCount = phj.ItemsCount
 
 	if phj.BlocksCount <= 0 {
-		return fmt.Errorf("part %q cannot contain zero blocks", partPath)
+		logger.Panicf("FATAL: part %q cannot contain zero blocks", partPath)
 	}
 	if phj.BlocksCount > phj.ItemsCount {
-		return fmt.Errorf("the number of blocks cannot exceed the number of items in the part %q; got blocksCount=%d, itemsCount=%d",
+		logger.Panicf("FATAL: the number of blocks cannot exceed the number of items in the part %q; got blocksCount=%d, itemsCount=%d",
 			partPath, phj.BlocksCount, phj.ItemsCount)
 	}
 	ph.blocksCount = phj.BlocksCount
 
 	ph.firstItem = append(ph.firstItem[:0], phj.FirstItem...)
 	ph.lastItem = append(ph.lastItem[:0], phj.LastItem...)
-
-	return nil
 }
 
 func (ph *partHeader) MustWriteMetadata(partPath string) {

lib/mergeset/block_stream_reader.go
@@ -17,7 +17,7 @@ type blockStreamReader struct {
 	// Block contains the current block if Next returned true.
 	Block inmemoryBlock
 
-	// isInmemoryBlock is set to true if bsr was initialized with InitFromInmemoryBlock().
+	// isInmemoryBlock is set to true if bsr was initialized with MustInitFromInmemoryBlock().
 	isInmemoryBlock bool
 
 	// The index of the current item in the Block, which is returned from CurrItem()
@@ -103,16 +103,16 @@ func (bsr *blockStreamReader) String() string {
 	return bsr.ph.String()
 }
 
-// InitFromInmemoryBlock initializes bsr from the given ib.
-func (bsr *blockStreamReader) InitFromInmemoryBlock(ib *inmemoryBlock) {
+// MustInitFromInmemoryBlock initializes bsr from the given ib.
+func (bsr *blockStreamReader) MustInitFromInmemoryBlock(ib *inmemoryBlock) {
 	bsr.reset()
 	bsr.Block.CopyFrom(ib)
 	bsr.Block.SortItems()
 	bsr.isInmemoryBlock = true
 }
 
-// InitFromInmemoryPart initializes bsr from the given mp.
-func (bsr *blockStreamReader) InitFromInmemoryPart(mp *inmemoryPart) {
+// MustInitFromInmemoryPart initializes bsr from the given mp.
+func (bsr *blockStreamReader) MustInitFromInmemoryPart(mp *inmemoryPart) {
 	bsr.reset()
 
 	var err error
@@ -134,18 +134,16 @@ func (bsr *blockStreamReader) InitFromInmemoryPart(mp *inmemoryPart) {
 	}
 }
 
-// InitFromFilePart initializes bsr from a file-based part on the given path.
+// MustInitFromFilePart initializes bsr from a file-based part on the given path.
 //
 // Part files are read without OS cache pollution, since the part is usually
 // deleted after the merge.
-func (bsr *blockStreamReader) InitFromFilePart(path string) error {
+func (bsr *blockStreamReader) MustInitFromFilePart(path string) {
 	bsr.reset()
 
 	path = filepath.Clean(path)
 
-	if err := bsr.ph.ReadMetadata(path); err != nil {
-		return fmt.Errorf("cannot read metadata from %q: %w", path, err)
-	}
+	bsr.ph.MustReadMetadata(path)
 
 	metaindexPath := filepath.Join(path, metaindexFilename)
 	metaindexFile := filestream.MustOpen(metaindexPath, true)
@@ -170,8 +168,6 @@ func (bsr *blockStreamReader) InitFromFilePart(path string) error {
 	bsr.indexReader = indexFile
 	bsr.itemsReader = itemsFile
 	bsr.lensReader = lensFile
-
-	return nil
 }
 
 // MustClose closes the bsr.

lib/mergeset/block_stream_writer.go
@@ -60,7 +60,7 @@ func (bsw *blockStreamWriter) reset() {
 	bsw.mrFirstItemCaught = false
 }
 
-func (bsw *blockStreamWriter) InitFromInmemoryPart(mp *inmemoryPart, compressLevel int) {
+func (bsw *blockStreamWriter) MustInitFromInmemoryPart(mp *inmemoryPart, compressLevel int) {
 	bsw.reset()
 
 	bsw.compressLevel = compressLevel

lib/mergeset/inmemory_part.go
@@ -6,7 +6,6 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/encoding"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fs"
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 )
 
 type inmemoryPart struct {
@@ -102,10 +101,7 @@ var inmemoryPartBytePool bytesutil.ByteBufferPool
 // It is unsafe re-using mp while the returned part is in use.
 func (mp *inmemoryPart) NewPart() *part {
 	size := mp.size()
-	p, err := newPart(&mp.ph, "", size, mp.metaindexData.NewReader(), &mp.indexData, &mp.itemsData, &mp.lensData)
-	if err != nil {
-		logger.Panicf("BUG: cannot create a part from inmemoryPart: %s", err)
-	}
+	p := newPart(&mp.ph, "", size, mp.metaindexData.NewReader(), &mp.indexData, &mp.itemsData, &mp.lensData)
 	return p
 }
 

lib/mergeset/merge_test.go
@@ -32,14 +32,14 @@ func TestMultilevelMerge(t *testing.T) {
 	// First level merge
 	var dstIP1 inmemoryPart
 	var bsw1 blockStreamWriter
-	bsw1.InitFromInmemoryPart(&dstIP1, -5)
+	bsw1.MustInitFromInmemoryPart(&dstIP1, -5)
 	if err := mergeBlockStreams(&dstIP1.ph, &bsw1, bsrs[:5], nil, nil, &itemsMerged); err != nil {
 		t.Fatalf("cannot merge first level part 1: %s", err)
 	}
 
 	var dstIP2 inmemoryPart
 	var bsw2 blockStreamWriter
-	bsw2.InitFromInmemoryPart(&dstIP2, -5)
+	bsw2.MustInitFromInmemoryPart(&dstIP2, -5)
 	if err := mergeBlockStreams(&dstIP2.ph, &bsw2, bsrs[5:], nil, nil, &itemsMerged); err != nil {
 		t.Fatalf("cannot merge first level part 2: %s", err)
 	}
@@ -56,7 +56,7 @@ func TestMultilevelMerge(t *testing.T) {
 		newTestBlockStreamReader(&dstIP1),
 		newTestBlockStreamReader(&dstIP2),
 	}
-	bsw.InitFromInmemoryPart(&dstIP, 1)
+	bsw.MustInitFromInmemoryPart(&dstIP, 1)
 	if err := mergeBlockStreams(&dstIP.ph, &bsw, bsrsTop, nil, nil, &itemsMerged); err != nil {
 		t.Fatalf("cannot merge second level: %s", err)
 	}
@@ -76,7 +76,7 @@ func TestMergeForciblyStop(t *testing.T) {
 	bsrs, _ := newTestInmemoryBlockStreamReaders(r, 20, 4000)
 	var dstIP inmemoryPart
 	var bsw blockStreamWriter
-	bsw.InitFromInmemoryPart(&dstIP, 1)
+	bsw.MustInitFromInmemoryPart(&dstIP, 1)
 	ch := make(chan struct{})
 	var itemsMerged uint64
 	close(ch)
@@ -125,7 +125,7 @@ func testMergeBlockStreamsSerial(r *rand.Rand, blocksToMerge, maxItemsPerBlock i
 	var itemsMerged uint64
 	var dstIP inmemoryPart
 	var bsw blockStreamWriter
-	bsw.InitFromInmemoryPart(&dstIP, -4)
+	bsw.MustInitFromInmemoryPart(&dstIP, -4)
 	if err := mergeBlockStreams(&dstIP.ph, &bsw, bsrs, nil, nil, &itemsMerged); err != nil {
 		return fmt.Errorf("cannot merge block streams: %w", err)
 	}
@@ -204,6 +204,6 @@ func newTestInmemoryBlockStreamReaders(r *rand.Rand, blocksCount, maxItemsPerBlo
 
 func newTestBlockStreamReader(ip *inmemoryPart) *blockStreamReader {
 	var bsr blockStreamReader
-	bsr.InitFromInmemoryPart(ip)
+	bsr.MustInitFromInmemoryPart(ip)
 	return &bsr
 }

lib/mergeset/part.go
@@ -1,7 +1,6 @@
 package mergeset
 
 import (
-	"fmt"
 	"path/filepath"
 	"sync"
 	"unsafe"
@@ -9,6 +8,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/blockcache"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/filestream"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fs"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/memory"
 )
 
@@ -67,11 +67,9 @@ type part struct {
 	lensFile  fs.MustReadAtCloser
 }
 
-func openFilePart(path string) (*part, error) {
+func mustOpenFilePart(path string) *part {
 	var ph partHeader
-	if err := ph.ReadMetadata(path); err != nil {
-		return nil, fmt.Errorf("cannot read part metadata: %w", err)
-	}
+	ph.MustReadMetadata(path)
 
 	metaindexPath := filepath.Join(path, metaindexFilename)
 	metaindexFile := filestream.MustOpen(metaindexPath, true)
@@ -93,11 +91,10 @@ func openFilePart(path string) (*part, error) {
 	return newPart(&ph, path, size, metaindexFile, indexFile, itemsFile, lensFile)
 }
 
-func newPart(ph *partHeader, path string, size uint64, metaindexReader filestream.ReadCloser, indexFile, itemsFile, lensFile fs.MustReadAtCloser) (*part, error) {
-	var errors []error
+func newPart(ph *partHeader, path string, size uint64, metaindexReader filestream.ReadCloser, indexFile, itemsFile, lensFile fs.MustReadAtCloser) *part {
 	mrs, err := unmarshalMetaindexRows(nil, metaindexReader)
 	if err != nil {
-		errors = append(errors, fmt.Errorf("cannot unmarshal metaindexRows: %w", err))
+		logger.Panicf("FATAL: cannot unmarshal metaindexRows from %q: %s", path, err)
 	}
 	metaindexReader.MustClose()
 
@@ -111,13 +108,7 @@ func newPart(ph *partHeader, path string, size uint64, metaindexReader filestrea
 	p.lensFile = lensFile
 
 	p.ph.CopyFrom(ph)
-	if len(errors) > 0 {
-		// Return only the first error, since it has no sense in returning all errors.
-		err := fmt.Errorf("error opening part %s: %w", p.path, errors[0])
-		p.MustClose()
-		return nil, err
-	}
-	return &p, nil
+	return &p
 }
 
 func (p *part) MustClose() {

lib/mergeset/part_search_test.go
@@ -151,7 +151,7 @@ func newTestPart(r *rand.Rand, blocksCount, maxItemsPerBlock int) (*part, []stri
 	var itemsMerged uint64
 	var ip inmemoryPart
 	var bsw blockStreamWriter
-	bsw.InitFromInmemoryPart(&ip, -3)
+	bsw.MustInitFromInmemoryPart(&ip, -3)
 	if err := mergeBlockStreams(&ip.ph, &bsw, bsrs, nil, nil, &itemsMerged); err != nil {
 		return nil, nil, fmt.Errorf("cannot merge blocks: %w", err)
 	}
@@ -159,9 +159,6 @@ func newTestPart(r *rand.Rand, blocksCount, maxItemsPerBlock int) (*part, []stri
 		return nil, nil, fmt.Errorf("unexpected itemsMerged; got %d; want %d", itemsMerged, len(items))
 	}
 	size := ip.size()
-	p, err := newPart(&ip.ph, "partName", size, ip.metaindexData.NewReader(), &ip.indexData, &ip.itemsData, &ip.lensData)
-	if err != nil {
-		return nil, nil, fmt.Errorf("cannot create part: %w", err)
-	}
+	p := newPart(&ip.ph, "partName", size, ip.metaindexData.NewReader(), &ip.indexData, &ip.itemsData, &ip.lensData)
 	return p, items, nil
 }

commit 5d862c690fa6f08be590a12d3d4806aad1acad26

lib/storage: fix a bug, which prevents from reading pre-v1.90.0 parts
The bug has been introduced in dfc26f3ea3c3862a626d703bc9829c0c6e857455

lib/storage/part_header.go
@@ -135,19 +135,20 @@ func (ph *partHeader) MustReadMetadata(partPath string) {
 	ph.Reset()
 
 	metadataPath := filepath.Join(partPath, metadataFilename)
-	metadata, err := os.ReadFile(metadataPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			// This is a part created before v1.90.0.
-			// Fall back to reading the metadata from the partPath itsel
-			if err := ph.ParseFromPath(partPath); err != nil {
-				logger.Panicf("FATAL: cannot parse metadata from %q: %s", partPath, err)
-			}
+	if !fs.IsPathExist(metadataPath) {
+		// This is a part created before v1.90.0.
+		// Fall back to reading the metadata from the partPath itsel
+		if err := ph.ParseFromPath(partPath); err != nil {
+			logger.Panicf("FATAL: cannot parse metadata from %q: %s", partPath, err)
 		}
-		logger.Panicf("FATAL: cannot read %q: %s", metadataPath, err)
-	}
-	if err := json.Unmarshal(metadata, ph); err != nil {
-		logger.Panicf("FATAL: cannot parse %q: %s", metadataPath, err)
+	} else {
+		metadata, err := os.ReadFile(metadataPath)
+		if err != nil {
+			logger.Panicf("FATAL: cannot read %q: %s", metadataPath, err)
+		}
+		if err := json.Unmarshal(metadata, ph); err != nil {
+			logger.Panicf("FATAL: cannot parse %q: %s", metadataPath, err)
+		}
 	}
 
 	// Perform various checks