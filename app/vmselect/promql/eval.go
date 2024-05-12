hanzoai/metrics

commit 2049cfb13dd734b5fc3b540a782529eea6781a44

app/vmselect: allow limiting per-query memory usage via -search.maxMemoryPerQuery command-line flag
Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3203

app/vmselect/promql/eval.go
@@ -15,6 +15,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/bytesutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/cgroup"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/decimal"
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/flagutil"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/memory"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/querytracer"
@@ -27,7 +28,12 @@ var (
 	disableCache                   = flag.Bool("search.disableCache", false, "Whether to disable response caching. This may be useful during data backfilling")
 	maxPointsSubqueryPerTimeseries = flag.Int("search.maxPointsSubqueryPerTimeseries", 100e3, "The maximum number of points per series, which can be generated by subquery. "+
 		"See https://valyala.medium.com/prometheus-subqueries-in-victoriametrics-9b1492b720b3")
-	noStaleMarkers = flag.Bool("search.noStaleMarkers", false, "Set this flag to true if the database doesn't contain Prometheus stale markers, so there is no need in spending additional CPU time on its handling. Staleness markers may exist only in data obtained from Prometheus scrape targets")
+	maxMemoryPerQuery = flagutil.NewBytes("search.maxMemoryPerQuery", 0, "The maximum amounts of memory a single query may consume. "+
+		"Queries requiring more memory are rejected. The total memory limit for concurrently executed queries can be estimated as "+
+		"-search.maxMemoryPerQuery multiplied by -search.maxConcurrentRequests . "+
+		"If the -search.maxMemoryPerQuery isn't set, then it is automatically calculated by dividing -memory.allowedPercent by -search.maxConcurrentRequests")
+	noStaleMarkers = flag.Bool("search.noStaleMarkers", false, "Set this flag to true if the database doesn't contain Prometheus stale markers, "+
+		"so there is no need in spending additional CPU time on its handling. Staleness markers may exist only in data obtained from Prometheus scrape targets")
 )
 
 // The minimum number of points per timeseries for enabling time rounding.
@@ -1051,20 +1057,18 @@ func evalRollupFuncWithMetricExpr(qt *querytracer.Tracer, ec *EvalConfig, funcNa
 		}
 	}
 	rollupPoints := mulNoOverflow(pointsPerTimeseries, int64(timeseriesLen*len(rcs)))
-	rollupMemorySize = mulNoOverflow(rollupPoints, 16)
-	rml := getRollupMemoryLimiter()
-	if !rml.Get(uint64(rollupMemorySize)) {
+	rollupMemorySize = sumNoOverflow(mulNoOverflow(int64(rssLen), 1000), mulNoOverflow(rollupPoints, 16))
+	maxMemory := getMaxMemoryPerQuery()
+	if rollupMemorySize > maxMemory {
 		rss.Cancel()
 		return nil, &UserReadableError{
-			Err: fmt.Errorf("not enough memory for processing %d data points across %d time series with %d points in each time series; "+
-				"total available memory for concurrent requests: %d bytes; "+
-				"requested memory: %d bytes; "+
-				"possible solutions are: reducing the number of matching time series; switching to node with more RAM; "+
-				"increasing -memory.allowedPercent; increasing `step` query arg (%gs)",
-				rollupPoints, timeseriesLen*len(rcs), pointsPerTimeseries, rml.MaxSize, uint64(rollupMemorySize), float64(ec.Step)/1e3),
+			Err: fmt.Errorf("not enough memory for processing %d data points across %d time series with %d points in each time series "+
+				"according to -search.maxMemoryPerQuery=%d; requested memory: %d bytes; "+
+				"possible solutions are: reducing the number of matching time series; increasing -search.maxMemoryPerQuery; "+
+				"increasing `step` query arg (%gs)",
+				rollupPoints, timeseriesLen*len(rcs), pointsPerTimeseries, maxMemory, rollupMemorySize, float64(ec.Step)/1e3),
 		}
 	}
-	defer rml.Put(uint64(rollupMemorySize))
 
 	// Evaluate rollup
 	keepMetricNames := getKeepMetricNames(expr)
@@ -1084,17 +1088,20 @@ func evalRollupFuncWithMetricExpr(qt *querytracer.Tracer, ec *EvalConfig, funcNa
 	return tss, nil
 }
 
-var (
-	rollupMemoryLimiter     memoryLimiter
-	rollupMemoryLimiterOnce sync.Once
-)
-
-func getRollupMemoryLimiter() *memoryLimiter {
-	rollupMemoryLimiterOnce.Do(func() {
-		rollupMemoryLimiter.MaxSize = uint64(memory.Allowed()) / 4
-	})
-	return &rollupMemoryLimiter
+func getMaxMemoryPerQuery() int64 {
+	if n := maxMemoryPerQuery.N; n > 0 {
+		return int64(n)
+	}
+	return maxMemoryPerQueryDefault
 }
+
+// InitMaxMemoryPerQuery must be called after flag.Parse and before promql usage.
+func InitMaxMemoryPerQuery(maxConcurrentRequests int) {
+	n := int(0.8*float64(memory.Allowed())) / maxConcurrentRequests
+	maxMemoryPerQueryDefault = int64(n)
+}
+
+var maxMemoryPerQueryDefault int64
 
 func evalRollupWithIncrementalAggregate(qt *querytracer.Tracer, funcName string, keepMetricNames bool,
 	iafc *incrementalAggrFuncContext, rss *netstorage.Results, rcs []*rollupConfig,
@@ -1225,6 +1232,14 @@ func mulNoOverflow(a, b int64) int64 {
 		return math.MaxInt64
 	}
 	return a * b
+}
+
+func sumNoOverflow(a, b int64) int64 {
+	if math.MaxInt64-a < b {
+		// Overflow
+		return math.MaxInt64
+	}
+	return a + b
 }
 
 func dropStaleNaNs(funcName string, values []float64, timestamps []int64) ([]float64, []int64) {

app/vmselect/main.go
@@ -59,6 +59,7 @@ func Init() {
 	fs.RemoveDirContents(tmpDirPath)
 	netstorage.InitTmpBlocksDir(tmpDirPath)
 	promql.InitRollupResultCache(*vmstorage.DataPath + "/cache/rollupResult")
+	promql.InitMaxMemoryPerQuery(*maxConcurrentRequests)
 
 	concurrencyCh = make(chan struct{}, *maxConcurrentRequests)
 	initVMAlertProxy()

app/vmselect/promql/memory_limiter.go
@@ -1,33 +1,0 @@
-package promql
-
-import (
-	"sync"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
-)
-
-type memoryLimiter struct {
-	MaxSize uint64
-
-	mu    sync.Mutex
-	usage uint64
-}
-
-func (ml *memoryLimiter) Get(n uint64) bool {
-	ml.mu.Lock()
-	ok := n <= ml.MaxSize && ml.MaxSize-n >= ml.usage
-	if ok {
-		ml.usage += n
-	}
-	ml.mu.Unlock()
-	return ok
-}
-
-func (ml *memoryLimiter) Put(n uint64) {
-	ml.mu.Lock()
-	if n > ml.usage {
-		logger.Panicf("BUG: n=%d cannot exceed %d", n, ml.usage)
-	}
-	ml.usage -= n
-	ml.mu.Unlock()
-}

app/vmselect/promql/memory_limiter_test.go
@@ -1,56 +1,0 @@
-package promql
-
-import (
-	"testing"
-)
-
-func TestMemoryLimiter(t *testing.T) {
-	var ml memoryLimiter
-	ml.MaxSize = 100
-
-	// Allocate memory
-	if !ml.Get(10) {
-		t.Fatalf("cannot get 10 out of %d bytes", ml.MaxSize)
-	}
-	if ml.usage != 10 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 10)
-	}
-	if !ml.Get(20) {
-		t.Fatalf("cannot get 20 out of 90 bytes")
-	}
-	if ml.usage != 30 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 30)
-	}
-	if ml.Get(1000) {
-		t.Fatalf("unexpected get for 1000 bytes")
-	}
-	if ml.usage != 30 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 30)
-	}
-	if ml.Get(71) {
-		t.Fatalf("unexpected get for 71 bytes")
-	}
-	if ml.usage != 30 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 30)
-	}
-	if !ml.Get(70) {
-		t.Fatalf("cannot get 70 bytes")
-	}
-	if ml.usage != 100 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 100)
-	}
-
-	// Return memory back
-	ml.Put(10)
-	ml.Put(70)
-	if ml.usage != 20 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 20)
-	}
-	if !ml.Get(30) {
-		t.Fatalf("cannot get 30 bytes")
-	}
-	ml.Put(50)
-	if ml.usage != 0 {
-		t.Fatalf("unexpected usage; got %d; want %d", ml.usage, 0)
-	}
-}

commit ac1cbfdef85522c55a797761d2c78ab8806015bd

app/vmselect/promql: properly estimate the needed amounts of memory for executing aggregate function over rollup function in incremental mode
Incremental aggregation processes only GOMAXPROCS time series at a time, so its' memory usage doesn't depend
on the number of input time series.

The issue has been introduced in 2049cfb13dd734b5fc3b540a782529eea6781a44

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/3203

app/vmselect/promql/eval.go
@@ -1729,7 +1729,7 @@ func evalRollupFuncNoCache(qt *querytracer.Tracer, ec *EvalConfig, funcName stri
 		}
 	}
 	rollupPoints := mulNoOverflow(pointsPerSeries, int64(timeseriesLen*len(rcs)))
-	rollupMemorySize := sumNoOverflow(mulNoOverflow(int64(rssLen), 1000), mulNoOverflow(rollupPoints, 16))
+	rollupMemorySize := sumNoOverflow(mulNoOverflow(int64(timeseriesLen), 1000), mulNoOverflow(rollupPoints, 16))
 	if maxMemory := int64(logQueryMemoryUsage.N); maxMemory > 0 && rollupMemorySize > maxMemory {
 		memoryIntensiveQueries.Inc()
 		requestURI := ec.GetRequestURI()