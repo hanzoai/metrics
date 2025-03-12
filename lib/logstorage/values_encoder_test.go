hanzoai/metrics

commit 7830361bfe8ae529ba36cc4c4a708fdaa02d1184

lib/logstorage: do not treat a string with leading zeros as a number at tryParseUint64
The "00123" string shouldn't be treated as 123 number.

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361

lib/logstorage/values_encoder.go
@@ -367,7 +367,7 @@ func TryParseTimestampRFC3339Nano(s string) (int64, bool) {
 	if digits > 9 {
 		return 0, false
 	}
-	n64, ok := tryParseUint64(s)
+	n64, ok := tryParseDateUint64(s)
 	if !ok {
 		return 0, false
 	}
@@ -394,7 +394,7 @@ func parseTimezoneOffset(s string) (int64, string, bool) {
 	if len(offsetStr) == 0 {
 		return 0, s, false
 	}
-	offsetNsecs, ok := tryParseTimezoneOffset(offsetStr)
+	offsetNsecs, ok := tryParseHHMM(offsetStr)
 	if !ok {
 		return 0, s, false
 	}
@@ -404,18 +404,17 @@ func parseTimezoneOffset(s string) (int64, string, bool) {
 	return offsetNsecs, s[:n], true
 }
 
-func tryParseTimezoneOffset(offsetStr string) (int64, bool) {
-	n := strings.IndexByte(offsetStr, ':')
-	if n < 0 {
+func tryParseHHMM(s string) (int64, bool) {
+	if len(s) != len("hh:mm") || s[2] != ':' {
 		return 0, false
 	}
-	hourStr := offsetStr[:n]
-	minuteStr := offsetStr[n+1:]
-	hours, ok := tryParseUint64(hourStr)
+	hourStr := s[:2]
+	minuteStr := s[3:]
+	hours, ok := tryParseDateUint64(hourStr)
 	if !ok || hours > 24 {
 		return 0, false
 	}
-	minutes, ok := tryParseUint64(minuteStr)
+	minutes, ok := tryParseDateUint64(minuteStr)
 	if !ok || minutes > 60 {
 		return 0, false
 	}
@@ -451,7 +450,7 @@ func tryParseTimestampISO8601(s string) (int64, bool) {
 		return 0, false
 	}
 	millisecondStr := s[:len("000")]
-	msecs, ok := tryParseUint64(millisecondStr)
+	msecs, ok := tryParseDateUint64(millisecondStr)
 	if !ok {
 		return 0, false
 	}
@@ -472,7 +471,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 		return 0, false, s
 	}
 	yearStr := s[:len("YYYY")]
-	n, ok := tryParseUint64(yearStr)
+	n, ok := tryParseDateUint64(yearStr)
 	if !ok || n < 1677 || n > 2262 {
 		return 0, false, s
 	}
@@ -484,7 +483,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 		return 0, false, s
 	}
 	monthStr := s[:len("MM")]
-	n, ok = tryParseUint64(monthStr)
+	n, ok = tryParseDateUint64(monthStr)
 	if !ok {
 		return 0, false, s
 	}
@@ -501,7 +500,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 		return 0, false, s
 	}
 	dayStr := s[:len("DD")]
-	n, ok = tryParseUint64(dayStr)
+	n, ok = tryParseDateUint64(dayStr)
 	if !ok {
 		return 0, false, s
 	}
@@ -513,7 +512,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 		return 0, false, s
 	}
 	hourStr := s[:len("HH")]
-	n, ok = tryParseUint64(hourStr)
+	n, ok = tryParseDateUint64(hourStr)
 	if !ok {
 		return 0, false, s
 	}
@@ -525,7 +524,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 		return 0, false, s
 	}
 	minuteStr := s[:len("MM")]
-	n, ok = tryParseUint64(minuteStr)
+	n, ok = tryParseDateUint64(minuteStr)
 	if !ok {
 		return 0, false, s
 	}
@@ -534,7 +533,7 @@ func tryParseTimestampSecs(s string) (int64, bool, string) {
 
 	// Parse second
 	secondStr := s[:len("SS")]
-	n, ok = tryParseUint64(secondStr)
+	n, ok = tryParseDateUint64(secondStr)
 	if !ok {
 		return 0, false, s
 	}
@@ -554,12 +553,51 @@ func tryParseUint64(s string) (uint64, bool) {
 	if len(s) == 0 || len(s) > len("18_446_744_073_709_551_615") {
 		return 0, false
 	}
+	if len(s) > 1 && s[0] == '0' {
+		// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361
+		return 0, false
+	}
+
 	n := uint64(0)
 	for i := 0; i < len(s); i++ {
 		ch := s[i]
 		if ch == '_' {
 			continue
 		}
+		if ch < '0' || ch > '9' {
+			return 0, false
+		}
+		if n > ((1<<64)-1)/10 {
+			return 0, false
+		}
+		n *= 10
+		d := uint64(ch - '0')
+		if n > (1<<64)-1-d {
+			return 0, false
+		}
+		n += d
+	}
+	return n, true
+}
+
+// tryParseDateUint64 parses s (which is a part of some timestamp) as uint64 value.
+func tryParseDateUint64(s string) (uint64, bool) {
+	if len(s) == 0 || len(s) > 9 {
+		return 0, false
+	}
+
+	if len(s) == 2 {
+		// fast path for two-digit number, which is used in hours, minutes and seconds
+		if s[0] < '0' || s[0] > '9' {
+			return 0, false
+		}
+		n := 10*uint64(s[0]-'0') + uint64(s[1]-'0')
+		return n, true
+	}
+
+	n := uint64(0)
+	for i := 0; i < len(s); i++ {
+		ch := s[i]
 		if ch < '0' || ch > '9' {
 			return 0, false
 		}
@@ -645,7 +683,7 @@ func tryParseIPv4(s string) (uint32, bool) {
 	if n <= 0 || n > 3 {
 		return 0, false
 	}
-	v, ok = tryParseUint64(s[:n])
+	v, ok = tryParseDateUint64(s[:n])
 	if !ok || v > 255 {
 		return 0, false
 	}
@@ -657,7 +695,7 @@ func tryParseIPv4(s string) (uint32, bool) {
 	if n <= 0 || n > 3 {
 		return 0, false
 	}
-	v, ok = tryParseUint64(s[:n])
+	v, ok = tryParseDateUint64(s[:n])
 	if !ok || v > 255 {
 		return 0, false
 	}
@@ -669,7 +707,7 @@ func tryParseIPv4(s string) (uint32, bool) {
 	if n <= 0 || n > 3 {
 		return 0, false
 	}
-	v, ok = tryParseUint64(s[:n])
+	v, ok = tryParseDateUint64(s[:n])
 	if !ok || v > 255 {
 		return 0, false
 	}
@@ -677,7 +715,7 @@ func tryParseIPv4(s string) (uint32, bool) {
 	s = s[n+1:]
 
 	// Parse octet 4
-	v, ok = tryParseUint64(s)
+	v, ok = tryParseDateUint64(s)
 	if !ok || v > 255 {
 		return 0, false
 	}

lib/logstorage/values_encoder_test.go
@@ -583,6 +583,9 @@ func TestTryParseFloat64_Failure(t *testing.T) {
 
 	// Minus in the middle of string isn't allowed
 	f("12-5")
+
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361
+	f("01")
 }
 
 func TestTryParseFloat64Exact_Success(t *testing.T) {
@@ -716,6 +719,10 @@ func TestTryParseUint64_Failure(t *testing.T) {
 	f("foo")
 	f("1.2")
 	f("1e3")
+
+	// uint with leading zeros shouldn't be parsed as uint
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361
+	f("0123")
 }
 
 func TestTryParseInt64_Success(t *testing.T) {
@@ -768,6 +775,10 @@ func TestTryParseInt64_Failure(t *testing.T) {
 	f("foo")
 	f("1.2")
 	f("1e3")
+
+	// int with leading zeros shouldn't be parsed as uint
+	// See https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361
+	f("-0123")
 }
 
 func TestMarshalUint8String(t *testing.T) {

lib/logstorage/parser.go
@@ -2423,25 +2423,9 @@ func getDayRangeArg(lex *lexer) (int64, string, error) {
 	if err != nil {
 		return 0, "", err
 	}
-	n := strings.IndexByte(argStr, ':')
-	if n < 0 {
-		return 0, "", fmt.Errorf("invalid format for day_range arg; want 'hh:mm'; got %q", argStr)
-	}
-	hoursStr := argStr[:n]
-	minutesStr := argStr[n+1:]
-
-	hours, ok := tryParseUint64(hoursStr)
+	offset, ok := tryParseHHMM(argStr)
 	if !ok {
-		return 0, "", fmt.Errorf("cannot parse hh from %q; expected format: 'hh:mm'", hoursStr)
-	}
-	minutes, ok := tryParseUint64(minutesStr)
-	if !ok {
-		return 0, "", fmt.Errorf("cannot parse mm from %q; expected format: 'hh:mm'", minutesStr)
-	}
-
-	offset := int64(hours*nsecsPerHour + minutes*nsecsPerMinute)
-	if offset < 0 {
-		offset = 0
+		return 0, "", fmt.Errorf("cannot parse %q as 'hh:mm'", argStr)
 	}
 	if offset >= nsecsPerDay {
 		offset = nsecsPerDay - 1

lib/logstorage/parser_test.go
@@ -169,7 +169,7 @@ func TestParseDayRange(t *testing.T) {
 	}
 
 	f("[00:00, 24:00]", 0, nsecsPerDay-1, 0)
-	f("[10:20, 125:00]", 10*nsecsPerHour+20*nsecsPerMinute, nsecsPerDay-1, 0)
+	f("[10:20, 24:00]", 10*nsecsPerHour+20*nsecsPerMinute, nsecsPerDay-1, 0)
 	f("(00:00, 24:00)", 1, nsecsPerDay-2, 0)
 	f("[08:00, 18:00)", 8*nsecsPerHour, 18*nsecsPerHour-1, 0)
 	f("[08:00, 18:00) offset 2h", 8*nsecsPerHour, 18*nsecsPerHour-1, 2*nsecsPerHour)

commit 88b0a6ce18b2f54f4c609a81b199b5ef6d3b6824

lib/logstorage: properly parse floating-point numbers with leading zeroes in fractional part
Parsing for floating-point numbers with leading zeroes such as 1.023, 1.00234 has been broken
in the commit 7830361bfe8ae529ba36cc4c4a708fdaa02d1184 .

Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8464
Updates https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8361

lib/logstorage/values_encoder.go
@@ -817,14 +817,24 @@ func tryParseFloat64Internal(s string, isExact bool) (float64, bool) {
 	}
 	sInt := s[:n]
 	sFrac := s[n+1:]
+
 	nInt, ok := tryParseUint64(sInt)
 	if !ok {
 		return 0, false
 	}
-	nFrac, ok := tryParseUint64(sFrac)
+
+	// Skip leading zeroes at sFrac, since tryParseUint64 rejects them.
+	// This fixes https://github.com/VictoriaMetrics/VictoriaMetrics/issues/8464
+	n = 0
+	for n < len(sFrac)-1 && sFrac[n] == '0' {
+		n++
+	}
+
+	nFrac, ok := tryParseUint64(sFrac[n:])
 	if !ok {
 		return 0, false
 	}
+
 	p10 := math.Pow10(strings.Count(sFrac, "_") - len(sFrac))
 	f := math.FMA(float64(nFrac), p10, float64(nInt))
 	if minus {

lib/logstorage/values_encoder_test.go
@@ -329,6 +329,7 @@ func TestTryParseDuration_Success(t *testing.T) {
 	// zero duration
 	f("0s", 0)
 	f("0.0w0d0h0s0.0ms", 0)
+	f("-0.0w0.00d0h0s0.0000ms", 0)
 	f("-0w", 0)
 
 	// positive duration
@@ -337,6 +338,8 @@ func TestTryParseDuration_Success(t *testing.T) {
 	f("1µs", nsecsPerMicrosecond)
 	f("1ns", 1)
 	f("1h", nsecsPerHour)
+	f("0.001h", 0.001*nsecsPerHour)
+	f("0.05h", 0.05*nsecsPerHour)
 	f("1.5d", 1.5*nsecsPerDay)
 	f("1.5w", 1.5*nsecsPerWeek)
 	f("2.5y", 2.5*nsecsPerYear)
@@ -535,6 +538,8 @@ func TestTryParseFloat64_Success(t *testing.T) {
 
 	f("0.345", 0.345)
 	f("-0.345", -0.345)
+	f("1.0234", 1.0234)
+	f("-12.0098", -12.0098)
 
 	// The maximum integer
 	f("9007199254740991", (1<<53)-1)